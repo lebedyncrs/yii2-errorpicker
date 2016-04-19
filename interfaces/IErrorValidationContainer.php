5890d9e35cb926a816dc775adf6719e0992edf99
Sergiy Lebedyn <lebedyncrs@yandex.ru>
2016-04-19 08:23:07 +0200

    add interface

---
+++ interfaces/IErrorValidationContainer.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace lebedyncrs\errorpicker\interfaces;
+
+/**
+ * @author Sergij Lebedyn<sergij.lebedyn@gmail.com>
+ */
+interface IErrorValidationContainer {
+
+    /**
+     * return all errors
+     * @return array
+     */
+    public function getErrors();
+
+    /**
+     * Add multiple or single errors.
+     * @param array $errors
+     * @param string $category name of wrapper to errors
+     */
+    public function addErrors(array $errors, $category = null, $index = false);
+
+    /**
+     * Check if errors exist
+     * @param string $category if this parameter in not empty then error checking will be executed in category 
+     * @return boolean if errors exist
+     */
+    public function hasErrors($category = null);
+}
